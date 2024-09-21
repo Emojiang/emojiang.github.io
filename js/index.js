33dd0de68aa5f5782463d3de428f1af47075b8e8
Emojiang <emojiang_@outlook.com>
2024-09-22 00:10:48 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ const scrollArrow = document.getElementById('scroll-arrow');
 
 // 监听页面滚动事件
 window.addEventListener('scroll', function() {
-    // 判断页面是否滚动超过20px
-    if (window.pageYOffset > 20) {
-        // 如果滚动超过20px，隐藏箭头
+    // 判断页面是否滚动超过120px
+    if (window.pageYOffset > 120) {
+        // 如果滚动超过120px，隐藏箭头
         scrollArrow.style.opacity = '0';
         scrollArrow.style.visibility = 'hidden';
     } else {
-        // 如果滚动不超过20px，显示箭头
+        // 如果滚动不超过120px，显示箭头
         scrollArrow.style.opacity = '1';
         scrollArrow.style.visibility = 'visible';
     }
